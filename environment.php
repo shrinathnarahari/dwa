62c56a05ef7d7f58f40f366fd93f7085867b04a0
Shrinath <shrinathnarahari@gmail.com>
2012-10-24 19:30:35 -0400

    env

--- environment.php
+++ environment.php
@@ -1,9 +1,9 @@
 <?php
 # LIVE
 
-#define('IN_PRODUCTION', TRUE);
-#define('DISPLAY_ERRORS', TRUE);
-#define('ENABLE_OUTGOING_EMAIL', TRUE);
+define('IN_PRODUCTION', TRUE);
+define('DISPLAY_ERRORS', TRUE);
+define('ENABLE_OUTGOING_EMAIL', TRUE);
 
 # Toggle this based on whether you want to connect to your local DB or your live DB
 define('REMOTE_DB', FALSE);
