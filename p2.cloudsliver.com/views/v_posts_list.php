bd41259acb66c0e700cfc5a2549585c3494d9f49
Shrinath <shrinathnarahari@gmail.com>
2012-11-03 12:36:54 -0400

    list

--- p2.cloudsliver.com/views/v_posts_list.php
+++ p2.cloudsliver.com/views/v_posts_list.php
@@ -1,7 +1,7 @@
  <div id="box1">
 <? foreach($posts as $post): ?>
 	
-	<h2><?=$post['first_name']?> <?=$post['last_name']?> posted:</h2>
+	<h2>List of posts <?=$post['first_name']?> <?=$post['last_name']?> <?=$post['count']?></h2>
 	<!--?=$post['content']?-->
 	
 	<br><br>
