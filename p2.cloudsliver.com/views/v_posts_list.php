1630272403b9f88dad5997f96c52827b14a10586
Shrinath <shrinathnarahari@gmail.com>
2012-11-03 13:08:31 -0400

    a

--- p2.cloudsliver.com/views/v_posts_list.php
+++ p2.cloudsliver.com/views/v_posts_list.php
@@ -1,7 +1,9 @@
  <div id="box1">
 <? foreach($posts as $post): ?>
+    $nr_of_posts = mysql_fetch_array($post);
+	echo $nr_of_posts
     Debug::dump($post," list");
-	<h2>List of posts <?=$post['first_name']?> <?=$post['last_name']?> </h2>
+	<h2>List of posts <?=$post['first_name']?> <?=$post['last_name']?>  </h2>
 	<!--?=$post['content']?-->
 	
 	<br><br>
